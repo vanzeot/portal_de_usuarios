6cb314ba7536efbc11705923cc7c4b5520b6ffdd
Otávio Vanzelotti <vanzeot@gmail.com>
2022-05-30 17:59:04 -0300

    DONE: - PEQUENA MUDANÇA NO CONFIGURE (AUTH) TO DO: - SecurityConfiguration.java ESTÁ DEPRECIADA - FRONT END

--- src/main/java/br/com/itss/portal/security/SecurityConfiguration.java
+++ src/main/java/br/com/itss/portal/security/SecurityConfiguration.java
@@ -19,7 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected  void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);//.passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
